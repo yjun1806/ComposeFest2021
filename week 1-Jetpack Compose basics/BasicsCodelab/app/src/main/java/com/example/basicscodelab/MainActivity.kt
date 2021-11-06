41f8256246d1b4bce71ade6f3d2790bb3658eb38
YJun <68216755+yjun1806@users.noreply.github.com>
2021-11-06 17:36:46 +0900

    [Week1] lab4까지 진행

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +20,7 @@ class MainActivity : ComponentActivity() {
             BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Android JetPack Compose!!")
                 }
             }
         }
@@ -26,7 +29,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary){
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
